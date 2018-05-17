8bad3dd56a6614f2f67426ffb9b7db6a1d8d6d44
Dung Phan <brianp.dung@utexas.edu>
2018-05-17 18:07:55 -0500

    Log scale, ramdomized energy gun has been implemented in this feature branch. Remember to merge this branch to develop.

--- include/LSPrimaryGeneratorAction.hh
+++ include/LSPrimaryGeneratorAction.hh
@@ -13,6 +13,10 @@
 #include "G4SystemOfUnits.hh"
 #include "Randomize.hh"
 #include "globals.hh"
+#include "TH2.h"
+#include "TH1.h"
+#include "TRandom3.h"
+#include <cstdlib>
 
 class G4ParticleGun;
 class G4Event;
@@ -25,12 +29,20 @@ public:
 
   // method from the base class
   virtual void GeneratePrimaries(G4Event*);
+  virtual std::vector<double> LogBins(int, double, double);
+  virtual void GunRandomEnergy(double&, double&);
 
   // method to access particle gun
   const G4ParticleGun* GetParticleGun() const { return fParticleGun; }
 
 private:
   G4ParticleGun*  fParticleGun; // pointer a to G4 gun class
+  G4double fGunEnergyCenterValue;
+  G4double fELowerEdge, fEUpperEdge;
+  TH2D* hSurf;
+  bool IsEnergySet;
+
+  TRandom3 fRND;
 };
 
 #endif
